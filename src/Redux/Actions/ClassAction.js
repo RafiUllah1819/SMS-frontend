b0e102095a7a0fe77668e342a919b7ace2f1cb58
rafiullah1819 <rafiullah372@gmail.com>
2024-12-20 19:34:26 +0500

    api changed

--- src/Redux/Actions/ClassAction.js
+++ src/Redux/Actions/ClassAction.js
@@ -1,11 +1,11 @@
 import { GET_SECTIONS, GET_CLASSES } from "../Constants/Constants";
-import { url } from "../api";
+import { url } from "../../api";
 import axios from "axios";
 
 const get_AllClasses = () => {
   return (dispatch) => {
     axios
-      .get("http://localhost:5000/classRoute")
+      .get(`${url}/classRoute`)
       //   .get(`${url}/classRoute`)
 
       .then((response) => dispatch(get_classes(response.data)))
