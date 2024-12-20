10b8b807fefc9c3a2323c5bf23c9fd9273d9813a
rafiullah1819 <rafiullah372@gmail.com>
2024-12-20 19:59:45 +0500

    changes in routes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "font-awesome/css/font-awesome.min.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
